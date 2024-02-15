156d4d283b751f3d1a20c371226889f3bb3796df
Wendy <139488528+WendyDarli@users.noreply.github.com>
2024-02-15 15:50:38 -0300

    changeNoteColor

---
+++ changeNoteColor.js
@@ -0,0 +1,6 @@
+//double click to change
+
+//loop through the array on each double click 
+colors = [pink, yellow, blue, purple, green, red, orage];
+
+const pink = mynote.style.backgroundColor = "pink";
