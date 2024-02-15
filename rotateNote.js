2a8c9c970d95a6769ad5dbe770c0a7275897c85d
Wendy <139488528+WendyDarli@users.noreply.github.com>
2024-02-15 15:50:15 -0300

    Update rotateNote.js

--- rotateNote.js
+++ rotateNote.js
@@ -2,7 +2,7 @@
   let rotation = 0;
   
   mynote.addEventListener("mousedown", (event) => {
-    if (event.button === 2) { // Verifica se o botão direito do mouse foi clicado
+    if (event.button === 2) {
         rotation += 0.2;
         mynote.style.boxShadow = "10px 10px 10.3px -1px rgba(0, 0, 0, 0.25)";
         if (rotation > 0.2) {
