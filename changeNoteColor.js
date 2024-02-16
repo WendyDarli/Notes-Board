1d9f9007e7caf562163835d7472a078d9ac8378e
Wendy <139488528+WendyDarli@users.noreply.github.com>
2024-02-16 13:40:54 -0300

    + change note color logic done

--- changeNoteColor.js
+++ changeNoteColor.js
@@ -1,6 +1,35 @@
-//double click to change
 
-//loop through the array on each double click 
-colors = [pink, yellow, blue, purple, green, red, orage];
+const mynoteheader = document.getElementById("mynoteheader");
+const notes = document.getElementsByClassName("note");
 
-const pink = mynote.style.backgroundColor = "pink";
+function Color(noteColor, headerColor){
+    this.noteColor = noteColor;
+    this.headerColor = headerColor;
+}
+
+const pink = new Color("pink", "#fba5b4");
+const yellow = new Color("#fffdc0", "#cbcd88");
+const blue = new Color("#c4d5f9", "#97b1e6");
+const purple = new Color("#d2c4f9", "#b29fe8");
+const green = new Color("#c4f9ce", "#98e596");
+const red = new Color("#f9c4c4", "#e59696");
+const orange = new Color("#f2c6ac", "#cd9a7c");
+
+const colors = [pink, yellow, blue, purple, green, red, orange];
+let currentColorIndex = 0;
+
+
+mynote.addEventListener("dblclick", (event) => {
+    
+    currentColorIndex = (currentColorIndex + 1) % colors.length; 
+
+    //apply color
+    mynote.style.backgroundColor = colors[currentColorIndex].noteColor; 
+    mynoteheader.style.backgroundImage = `linear-gradient(${colors[currentColorIndex].headerColor}, ${colors[currentColorIndex].noteColor})`;
+    
+    for (let note of notes) {
+        note.style.backgroundColor = colors[currentColorIndex].noteColor;
+    }
+      
+});
+
