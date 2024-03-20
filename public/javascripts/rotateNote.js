4cc69d8829ed0dcfe775519cefce0374c5eb8ae5
Wendy <139488528+WendyDarli@users.noreply.github.com>
2024-03-20 13:19:33 -0300

    All notes now can be rotated
    -  apply the event for all indexes

--- public/javascripts/rotateNote.js
+++ public/javascripts/rotateNote.js
@@ -1,17 +1,20 @@
-const mynote = document.getElementsByClassName("mynote")[0];
+const mynotes = document.getElementsByClassName("mynote");
 let rotation = 0;
-  
-  mynote.addEventListener("mousedown", (event) => {
-    if (event.button === 2) {
-        rotation += 0.2;
-        mynote.style.boxShadow = "10px 10px 10.3px -1px rgba(0, 0, 0, 0.25)";
-        if (rotation > 0.2) {
-            rotation = -0.2; 
-            mynote.style.boxShadow = "-10px 10px 10.3px -1px rgba(0, 0, 0, 0.25)";
-        }
-        mynote.style.transform = `rotate(${rotation}rad)`;
-    }
-});
 
-//prevent context menu from appearing
+for (let i = 0; i < mynotes.length; i++) {
+    mynotes[i].addEventListener("mousedown", function(event) {
+        
+        if (event.button === 2) { 
+            rotation += 0.2;
+            this.style.boxShadow = "10px 10px 10.3px -1px rgba(0, 0, 0, 0.25)";
+            if (rotation > 0.2) {
+                rotation = -0.2;
+                this.style.boxShadow = "-10px 10px 10.3px -1px rgba(0, 0, 0, 0.25)";
+            }
+            this.style.transform = `rotate(${rotation}rad)`;
+        }
+    });
+};
+
+//prevent context menu from appearing when doublecklick
 window.addEventListener(`contextmenu`, (e) => e.preventDefault());
